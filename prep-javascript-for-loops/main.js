3bdc81cf3c117bca02b33a7f852e1577a3cd15a7
Dennis-YJ-Chang <dennisyjchang@gmail.com>
2021-08-12 21:35:59 -0700

    Completed prep-javascript-for-loops

---
+++ prep-javascript-for-loops/main.js
@@ -0,0 +1,12 @@
+// create your loops here.
+for (var i = 0; i < 10; i++) {
+  console.log(i);
+}
+
+for (var i = 0; i < 18; i = i + 2) {
+  console.log(i);
+}
+
+for (var i = 100; i > 0; i = i - 1) {
+  console.log('Time till explosion:', i);
+}
